8df54e8d207b8baac667139a138b78acb2556357
Rajeev Gurram <118657580+Rajeev973@users.noreply.github.com>
2023-08-01 07:43:12 +0100

    Add files via upload

---
+++ Task 2/Task 2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from mrjob.job import MRJob
+import re
+
+STOPWORDS = set(['the', 'and', 'of', 'a', 'to', 'in', 'is', 'it'])
+WORD_RE = re.compile(r"[\w']+")
+
+class MRWordCountWithoutStopwords(MRJob):
+
+    def mapper(self, _, line):
+        for word in WORD_RE.findall(line):
+            word = word.lower()
+            if word not in STOPWORDS:
+                yield word, 1
+
+    def reducer(self, word, counts):
+        yield word, sum(counts)
+
+if __name__ == '__main__':
+    MRWordCountWithoutStopwords.run()
+
+
+# In[ ]:
+
+
+
+
